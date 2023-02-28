964723ae25d9eae3032cf35524334dc0bcbb6fad
Eduardo Xavier <eduardosvx@gmail.com>
2023-02-28 16:37:06 -0300

    feature/navbar-navmenu

---
+++ frontend/src/Pages/colaborador/historico_solicitacoes.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function historico_solicitacoes() {
+  return (
+    <div>
+        <h1>Histórico de solicitações</h1>
+    </div>
+  )
+}
+
+export default historico_solicitacoes
---
+++ frontend/src/Pages/colaborador/solicitacoes.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function solicitacoes() {
+  return (
+    <div>
+        <h1>solicitacoes</h1>
+    </div>
+  )
+}
+
+export default solicitacoes
